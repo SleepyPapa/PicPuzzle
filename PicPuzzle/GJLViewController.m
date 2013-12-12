76e6d4e70767a152a31944c6998919cfb74c24f7
Gregory Lavery <gregory.lavery@yahoo.com>
2013-12-12 11:07:36 -0800

    This is a version which uses autogenerated array sizes to create massive puzzles, fixed sizing issue on icons so grid is always right size.

--- PicPuzzle/GJLViewController.m
+++ PicPuzzle/GJLViewController.m
@@ -107,7 +107,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    _numberOfTiles=5;
+    _numberOfTiles=4;
     [self setUpAllTheLocations];
     
 }
@@ -122,61 +122,6 @@
     CGSize insetSize = CGRectInset(self.view.frame, 30, 80).size;
     CGFloat xUnits = (insetSize.width/numberInGrid);
     CGFloat yUnits = (insetSize.height/numberInGrid);
-
-    NSArray* tileArray = @[@"tile1",
-                           @"tile2",
-                           @"tile3",
-                           @"tile4",
-                           @"tile5",
-                           @"tile6",
-                           @"tile7",
-                           @"tile8",
-                           @"tile9",
-                           @"tile10",
-                           @"tile11",
-                           @"tile12",
-                           @"tile13",
-                           @"tile14",
-                           @"tile15",
-                           @"tile6",
-                           @"tile7",
-                           @"tile8",
-                           @"tile9",
-                           @"tile10",
-                           @"tile11",
-                           @"tile12",
-                           @"tile13",
-                           @"tile14",
-                           @"tile_blank"];
-    
-    NSArray* locationArray = @[@"location1",
-                           @"location2",
-                           @"location3",
-                           @"location4",
-                               @"location5",
-                               @"location6",
-                               @"location7",
-                               @"location8",
-                               @"location9",
-                               @"location10",
-                               @"location11",
-                               @"location12",
-                               @"location13",
-                               @"location14",
-                               @"location15",
-                               @"location16",
-                               @"location17",
-                               @"location18",
-                               @"location19",
-                               @"location20",
-                               @"location21",
-                               @"location22",
-                               @"location23",
-                               @"location24",
-                           @"blank"];
-    
-
-
     CGFloat positionX = 0.0f;
     CGFloat positionY = 0.0f;
     int rowNumber=0;
@@ -184,17 +129,20 @@
     
     _whereInGrid = [[NSMutableArray alloc] init];
     _storedLocations = [[NSMutableArray alloc] init];
-//    [self setNumberOfTiles:numberInGrid];
+
 
 	for (NSInteger i = 0; i < (totalNumberOfTiles); i++)
 	{
-		NSString *whichTile = [tileArray objectAtIndex:(i)];
-        NSString *fileName = [whichTile stringByAppendingString:@".png"];
+        NSString *fileName;
+        if (i%2==0)
+             fileName= @"red.png";
+        else
+             fileName= @"black.png";
         UIImage *toLoad;
         if (i!=(totalNumberOfTiles-1))
             toLoad = [UIImage imageNamed:fileName];
         else {
-            toLoad = [UIImage imageNamed:@"tile_blank.png"];
+            toLoad = [UIImage imageNamed:@"white.png"];
         }
         int result = i%numberInGrid;
         if (result==0)
@@ -205,24 +153,23 @@
         GJLTileButton *individualTile = [GJLTileButton button];
 
         [individualTile setBackgroundImage:toLoad forState:UIControlStateNormal];
-//        [individualTile setImage:toLoad forState:UIControlStateHighlighted];
-        individualTile.adjustsImageWhenHighlighted=YES; //hack2
+
+        individualTile.adjustsImageWhenHighlighted=YES;
 
         individualTile.titleLabel.font = [UIFont boldSystemFontOfSize:12.0f];
         NSString *tileUIName =[NSString stringWithFormat:@"%d",(i+1)];
         [individualTile setTitle:tileUIName forState:UIControlStateNormal];
-//        CGPoint positionOfFrame = CGPointMake(positionX,positionY);
+
         individualTile.center = CGPointMake(positionX,positionY);
-        individualTile.tileName=[locationArray objectAtIndex:(i)];
-        [_whereInGrid addObject:[locationArray objectAtIndex:(i)]];
+        individualTile.bounds = CGRectMake(positionX, positionY, xUnits,yUnits);
+        NSString *locationName = [NSString stringWithFormat:@"location%d",(i+1)];
+
+        individualTile.tileName=locationName;
+        [_whereInGrid addObject:locationName];
         individualTile.tag = i+1;
         [_storedLocations addObject:[NSValue valueWithCGPoint:individualTile.center]];
         individualTile.contentMode = UIViewContentModeScaleAspectFit;
-//        individualTile.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
-		
-//        [imageView setCenter:positionOfFrame];
-        
-//        imageView.userInteractionEnabled=YES;
+
         individualTile.userInteractionEnabled=YES;
         if (i==(totalNumberOfTiles-1)){
             individualTile.tileName=@"tile_blank";
